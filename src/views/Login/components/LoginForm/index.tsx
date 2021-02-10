c231d7677df4466a2f7e82b6d9c05c037abff611
laixinyu <laixinyu@gmail.com>
2021-02-10 15:30:21 +0800

    Add 拆分 LoginForm 组件

--- src/views/Login/components/LoginForm/index.tsx
+++ src/views/Login/components/LoginForm/index.tsx
@@ -1,20 +1,49 @@
+import { useState, useEffect } from 'react';
+
 interface LoginFormProps {
-    account?: string,
-    password?: string
+    account?: string | undefined,
+    password?: string | undefined
 }
 
-export function LoginForm(props: LoginFormProps):JSX.Element {
-    console.log(props.account);
-    console.log(props.password);
+export function LoginForm(props: LoginFormProps): JSX.Element {
+    let [account, setAccount] = useState<string | undefined>(props.account);
+    let [password, setPassword] = useState<string | undefined>(props.password);
+
+    const verifydRule = {
+        account: {
+            required: true,
+            max: 10,
+            min: 5,
+            test: []
+        },
+        password: []
+    };
+
+    useEffect(() => {
+
+    }, [account, password]);
+
+    function onChange({target}: { target: HTMLInputElement }): void {
+        const {name, value} = target;
+
+        if (name === 'account') {
+            setAccount(value);
+        }
+
+        if (name === 'password') {
+            setPassword(value);
+        }
+    }
+
     return (
         <div className="login-form">
             <div className="login-form-item">
                 <div className="login-form-label" />
-                <input type="text" />
+                <input type="text" name="account" onChange={onChange} defaultValue={account} />
             </div>
             <div className="login-form-item">
                 <div className="login-form-label" />
-                <input type="text" />
+                <input type="text" name="password" onChange={onChange} defaultValue={password} />
             </div>
         </div>
     );
